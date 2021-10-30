fa2af61cb88b99cdc4212ce3524b501cac536868
moojing <starts789@gmail.com>
2021-10-30 12:23:16 +0800

    feat: fetch the asets list and show assets with card component.

--- src/apis/index.js
+++ src/apis/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = "https://api.opensea.io";
 const WALLET_ADDRESS = "0x960DE9907A2e2f5363646d48D7FB675Cd2892e91";
 
-export const apiFetchAssets = ({ offset, limit }) =>
+export const apiFetchAssets = ({ offset = 0, limit = 2 }) =>
   axios({
     method: "get",
     url: `${BASE_URL}/api/v1/assets`,
@@ -13,10 +13,10 @@ export const apiFetchAssets = ({ offset, limit }) =>
       offset,
       limit,
     },
-  });
+  }).then((res) => res.data);
 
 export const apiFetchAssetDetail = (tokenId) =>
   axios({
     method: "get",
     url: `${BASE_URL}/api/v1/asset/${WALLET_ADDRESS}/${tokenId}`,
-  });
+  }).then((res) => res.data);
--- src/components/AssetsList/index.js
+++ src/components/AssetsList/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+import { apiFetchAssets } from "apis";
 // import styles from "./AssetsList.scss";
 function AssetsList() {
-  return <div> AssetsList </div>;
+  const [assetsList, setAssetsList] = useState([]);
+  useEffect(() => {
+    apiFetchAssets({ offset: 0, limit: 2 }).then(({ assets }) => {
+      console.log(assets);
+      setAssetsList(assets);
+    });
+  }, []);
+
+  const renderAssetCard = ({ id, image_url, name }) => {
+    return (
+      <Card sx={{ maxWidth: 320 }} key={id}>
+        <CardMedia component="img" height="200" image={image_url} alt={name} />
+        <CardContent>
+          <Typography gutterBottom variant="h4" component="div">
+            {name}
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+  };
+
+  return <div>{assetsList.map(renderAssetCard)}</div>;
 }
 AssetsList.propTypes = {};
 export default AssetsList;

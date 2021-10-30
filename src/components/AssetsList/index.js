529da72f61309180808cffbd4e67dc0a6dbb7c85
moojing <starts789@gmail.com>
2021-10-30 15:11:01 +0800

    fix: page offset value.

--- src/components/AssetsList/index.js
+++ src/components/AssetsList/index.js
@@ -10,18 +10,22 @@ import Container from "@mui/material/Container";
 
 import useScrollBottom from "hooks/useScrollBottom";
 import { apiFetchAssets } from "apis";
+const PAGE_SIZE = 2;
 function AssetsList() {
   const history = useHistory();
   const [noMoreAssets, setNoMoreAssets] = useState(false);
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPageOffset, setCurrentPageOffsetOffset] = useState(0);
   const [assetsList, setAssetsList] = useState([]);
+  console.log("assetsList :", assetsList);
   useEffect(() => {
-    apiFetchAssets({ offset: currentPage, limit: 2 }).then(({ assets }) => {
-      if (!assets.length) setNoMoreAssets(true);
+    apiFetchAssets({ offset: currentPageOffset, limit: PAGE_SIZE }).then(
+      ({ assets }) => {
+        if (!assets.length) setNoMoreAssets(true);
 
-      setAssetsList((prev) => [...prev, ...assets]);
-    });
-  }, [currentPage]);
+        setAssetsList((prev) => [...prev, ...assets]);
+      }
+    );
+  }, [currentPageOffset]);
 
   const onClickCard = (contractAddress, tokenId) => () => {
     history.push(`/detail/${contractAddress}/${tokenId}`);
@@ -29,7 +33,7 @@ function AssetsList() {
 
   const onScrollBottom = useCallback(() => {
     if (noMoreAssets) return;
-    setCurrentPage((prev) => prev + 1);
+    setCurrentPageOffsetOffset((prev) => prev + PAGE_SIZE);
   }, [noMoreAssets]);
 
   useScrollBottom(onScrollBottom);
@@ -43,7 +47,7 @@ function AssetsList() {
   }) => {
     const contractAddress = asset_contract.address;
     return (
-      <Grid item xs={12} sm={6} md={4} lg={3} key={id}>
+      <Grid item xs={12} sm={6} md={4} lg={3} key={`${id}${token_id}`}>
         <Card
           sx={{ cursor: "pointer" }}
           onClick={onClickCard(contractAddress, token_id)}

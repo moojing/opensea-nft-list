bffdccbf4a0f9581b997f45aa1b54936d29397f3
moojing <starts789@gmail.com>
2021-10-30 13:43:40 +0800

    feat: add backIcon for narbar.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,19 +1,46 @@
 import * as React from "react";
+import { string, bool } from "prop-types";
+import { useHistory } from "react-router-dom";
+
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Typography from "@mui/material/Typography";
 
-export default function NavBar() {
+export default function Navbar({ showBackIcon, title }) {
+  const history = useHistory();
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
+          {showBackIcon && (
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              sx={{ mr: 2 }}
+              onClick={() => history.goBack()}
+            >
+              <ArrowBackIcon />
+            </IconButton>
+          )}
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            NFT List Demo
+            {title || "NFT List Demo"}
           </Typography>
         </Toolbar>
       </AppBar>
     </Box>
   );
 }
+
+Navbar.propTypes = {
+  title: string,
+  showBackIcon: bool,
+};
+
+Navbar.defaultProps = {
+  title: "",
+  showBackIcon: false,
+};

7c90c3f433df15fb7fe5c9709c84a9c3d7796ef6
moojing <starts789@gmail.com>
2021-10-30 14:04:48 +0800

    feat: add card button and fake description.

--- src/components/AssetDetail/index.js
+++ src/components/AssetDetail/index.js
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { apiFetchAssetDetail } from "apis";
 import Navbar from "components/Navbar";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
-// import styles from "./AssetDetail.module.scss";
 function AssetDetail() {
   const { contractAddress, tokenId } = useParams();
   const [assetDetail, setAssetDetail] = useState({
@@ -32,7 +36,62 @@ function AssetDetail() {
   return (
     <>
       <Navbar showBackIcon title={assetDetail.collectionName} />
-      <Container maxWidth="sm">Detail</Container>
+      <Container
+        maxWidth="sm"
+        sx={{
+          my: 4,
+        }}
+      >
+        <Card>
+          <CardMedia
+            component="img"
+            image={assetDetail.imageUrl}
+            alt={assetDetail.name}
+          />
+          <CardContent>
+            <Typography
+              variant="h5"
+              component="div"
+              sx={{
+                minHeight: 64,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              {assetDetail.name}
+            </Typography>
+
+            <p>
+              {assetDetail.description} Lorem ipsum dolor sit amet consectetur,
+              adipisicing elit. Officia voluptatum accusantium beatae quibusdam,
+              recusandae iste sint a illo exercitationem repudiandae deleniti
+              incidunt minima expedita quia iure minus sequi? Optio, non. Lorem
+              ipsum dolor sit amet consectetur, adipisicing elit. Officia
+              voluptatum accusantium beatae quibusdam, recusandae iste sint a
+              illo exercitationem repudiandae deleniti incidunt minima expedita
+              quia iure minus sequi? Optio, non. Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Officia voluptatum accusantium
+              beatae quibusdam, recusandae iste sint a illo exercitationem
+              repudiandae deleniti incidunt minima expedita quia iure minus
+              sequi? Optio, non. itationem repudiandae deleniti incidunt minima
+              expedita quia iure minus sequi? Optio, non. Lorem ipsum dolor sit
+              amet consectetur, adipisicing elit. Officia voluptatum accusantium
+              beatae quibusdam, recusandae iste sint a illo exercitationem
+              repudiandae deleniti incidunt minima expedita quia iure minus
+              sequi? Optio, non itationem repudiandae deleniti incidunt minima
+              expedita quia iure minus sequi? Optio, non. Lorem ipsum dolor sit
+              amet consectetur, adipisicing elit. Officia voluptatum accusantium
+              beatae quibusdam, recusandae iste sint a illo exercitationem
+              repudiandae deleniti incidunt minima expedita quia iure minus
+              sequi? Optio, non
+            </p>
+          </CardContent>
+          <Button variant="contained" href={assetDetail.permalink} fullWidth>
+            Permalink
+          </Button>
+        </Card>
+      </Container>
     </>
   );
 }

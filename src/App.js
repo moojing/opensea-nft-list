c0bdd136bf461564dcf48f906d791449b12e4c03
moojing <starts789@gmail.com>
2021-10-30 11:55:43 +0800

    chore: set baseUrl in jsconfig.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Container from "@mui/material/Container";
+import Navbar from "components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Container>content</Container>
     </div>
   );
